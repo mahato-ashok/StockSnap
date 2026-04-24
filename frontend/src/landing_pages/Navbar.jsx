491b423887a6b034e30373ffefe43d02852d793b
Ashok Kumar Mahato <ashok79911@gmail.com>
2026-04-24 11:08:55 +0530

    Created the about page

--- frontend/src/landing_pages/Navbar.jsx
+++ frontend/src/landing_pages/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav class="navbar navbar-expand-lg  p-3 border-bottom">
       <div class="container-fluid">
-        <img src="media/images/logo.svg" alt="logo" style={{width:"10%"}}></img>
+        <img src="media/images/logo.svg" alt="logo" style={{width:"10%"}} ></img>
           <form class="d-flex " role="search">
           
              <ul class="navbar-nav me-auto mb-2 mb-lg-0 p-2">
