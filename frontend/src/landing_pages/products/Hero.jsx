ea06e9f8d54f55eb83c844513179692f599c353f
Ashok Kumar Mahato <ashok79911@gmail.com>
2026-04-25 20:08:16 +0530

    created the left and hero section

--- frontend/src/landing_pages/products/Hero.jsx
+++ frontend/src/landing_pages/products/Hero.jsx
@@ -2,6 +2,12 @@ import react from 'react'
 
 export default function Hero(){
     return (
-        <h1>hero</h1>
+        <div className='container border-bottom'>
+            <div className="row text-center my-5 p-3">
+                <h1>Technology</h1>
+                <p className='text-muted my-3'>Sleek,modern and intutive trading platforms</p>
+                <p>Check out our <a style={{textDecoration:"None"}}href='#'>investment offerings <i class="fa-solid fa-arrow-right"></i></a></p>
+            </div>
+        </div>
     )
 }
--- frontend/src/landing_pages/products/ProductsPage.jsx
+++ frontend/src/landing_pages/products/ProductsPage.jsx
@@ -11,10 +11,16 @@ export default function ProductsPage(){
     return (
         <>
     
-        <Universe/>
-        <LeftSection/>
-        <RightSection/>
+        
         <Hero/>
+        <LeftSection imageURL='media/images/kite.png' imageTitle='Kite' imageDescription='Our ultra-fast flagship trading platform with streaming market data,advanced charts, an elegant UI, and more. Enjoy the kite experience seamlessly on your Android and ios devices.' tryDemo='#' learnMore='#' playStore='#' appStore='#' />
+        <RightSection/>
+        <LeftSection imageURL='media/images/coin.png' imageTitle='Coin' imageDescription='Buy direct mutual funds online,commission-free, delivered directly to your demat account. Enjoy the investment experience on your Android and iOS devices' tryDemo='#' learnMore='#' playStore='#' appStore='#' />
+        <RightSection/>
+        <LeftSection imageURL='media/images/varsity.png' imageTitle='Varsity mobile' imageDescription='An easy to grasp,collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go.' tryDemo='#' learnMore='#' playStore='#' appStore='#' />
+        <RightSection/>
+        <Universe/>
+        
 
         </>
     )
