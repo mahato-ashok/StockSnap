bf0852cb2e81a7c40ec649dae833935dff11a692
mahato-ashok <ashok7991@gmail.com>
2026-05-01 20:32:17 +0530

    added components

--- frontend/src/landing_pages/support/CreateTicket.jsx
+++ frontend/src/landing_pages/support/CreateTicket.jsx
@@ -2,6 +2,79 @@ import react from 'react'
 
 export default function CreateTicket(){
     return(
-        <h1>create ticket</h1>
+        <div className="container">
+            <div className="row mt-5 p-5 ">
+                <h3>To create a ticket,select a relevant topic</h3>
+            </div>
+            <div className="row p-5">
+                 <div className="col p-5">
+                    <h4 className='mb-5'><i class="fa-solid fa-circle-plus fa-xs"></i> &nbsp;&nbsp;Account Opening</h4>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}} >Online Account Opening </a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Offline Account Opening</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Company,Partenrship and HUF Account Opening</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>NRI Account Opening</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Charges at Zerodha</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Zerodha IDFC First Bank 3-in-1 Account </a><br/>
+                     <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Getting started </a><br/>
+                 </div>
+                <div className="col p-5">
+                    <h4 className='mb-5'><i class="fa-solid fa-person"></i> &nbsp;&nbsp;Your Zerodha Account</h4>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}} >Login Credentials</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Account Modification and Segment Addition</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>DP ID and bank details</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Your profile</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Transfer and conversion of shares</a><br/>
+                 </div>
+                <div className="col p-5">
+                    <h4 className='mb-5'><i class="fa-solid fa-circle-plus fa-xs"></i> &nbsp;&nbsp;Your Zerodha Account</h4>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}} >Margin/leverage, product and order types </a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Kite Web and Mobile</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Trading FAQs</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Corporate Actions</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Senitel</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Sentinel</a><br/>
+                     <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Kite API</a><br/>
+                     <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Pi and other platforms</a><br/>
+                     <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Stockreports+</a><br/>
+                     <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>GTT</a><br/>
+                 </div>
+                 
+            </div>
+            <div className="row p-5">
+                 <div className="col p-5">
+                    <h4 className='mb-5'><i class="fa-solid fa-circle-plus fa-xs"></i> &nbsp;&nbsp;Funds</h4>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}} >Online Account Opening </a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Offline Account Opening</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Company,Partenrship and HUF Account Opening</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>NRI Account Opening</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Charges at Zerodha</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Zerodha IDFC First Bank 3-in-1 Account </a><br/>
+                     <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Getting started </a><br/>
+                 </div>
+                <div className="col p-5">
+                    <h4 className='mb-5'><i class="fa-solid fa-circle-plus fa-xs"></i> &nbsp;&nbsp;Console</h4>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}} >Login Credentials</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Account Modification and Segment Addition</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>DP ID and bank details</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Your profile</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Transfer and conversion of shares</a><br/>
+                 </div>
+                <div className="col p-5">
+                    <h4 className='mb-5'><i class="fa-solid fa-circle-plus fa-xs"></i> &nbsp;&nbsp;Coin</h4>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}} >Margin/leverage, product and order types </a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Kite Web and Mobile</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Trading FAQs</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Corporate Actions</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Senitel</a><br/>
+                    <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Sentinel</a><br/>
+                     <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Kite API</a><br/>
+                     <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Pi and other platforms</a><br/>
+                     <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>Stockreports+</a><br/>
+                     <a href='#'style={{textDecoration:"None",lineHeight:"2rem"}}>GTT</a><br/>
+                 </div>
+                 
+            </div>
+
+        </div>
     )
 }
